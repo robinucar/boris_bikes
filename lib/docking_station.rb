375e8e7dd1af16fc8e3c759f1abe9c20ad99d118
robinucar <mehmet.ucar@imediapro.co.uk>
2021-12-10 17:15:54 +0000

    defined full and empth methodes with private keyword

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -10,12 +10,20 @@ class DockingStation
   def initialize
     @bikes = []
   end
+  
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
   def dock_bike(bike)
-    fail 'Docking station full' if @bikes.count >=20
+    fail 'Docking station full' if full?
     @bikes << bike
+  end
+  private
+  def full?
+    @bikes.count >=20
+  end
+  def empty?
+    @bikes.empty?
   end
 end
